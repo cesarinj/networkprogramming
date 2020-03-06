40d1ccd0588bb1265e03c8fe3c98d14726dddf00
Cesar Aguirre <cesarinj@hotmail.com>
2020-03-06 16:55:26 -0500

    busqueda modulo

--- modulo.py
+++ modulo.py
@@ -2,19 +2,19 @@
 
 from netmiko import ConnectHandler
 
-switch_3com= {  "SWCONTDC" : "10.100.100.113",
+switch_3com= {  "SWCONTDC" : "10.10.10.10",
           }
-switch_3com1= { "SW3PCTAL" : "10.100.100.132",
+switch_3com1= { "SW3PCTAL" : "10.10.10.11",
 
             }
-switch_cisco= { "SWBRCSHM" : "10.100.100.121",  # Recurss humanos  9300
-                "SW33DIDT" : "10.100.100.124",  # JQUIROZ  9300
+switch_cisco= { "SWBRCSHM" : "10.10.10.12",  # Recurss humanos  9300
+                "SW33DIDT" : "10.10.10.13",  # JQUIROZ  9300
  #               "SW1PRLPB" : "10.100.100.126",   #cat 9300 Relaciones publicas 
-                "SW1PCPMT" : "10.100.100.134", #ok Laboratorio Cómputo 9
-                "SW3PLB42" : "10.100.100.135", # ws WS-C3560-24PS-E
+                "SW1PCPMT" : "10.10.10.14", #ok Laboratorio Cómputo 9
+                "SW3PLB42" : "10.10.10.15", # ws WS-C3560-24PS-E
                 }
     
-trasceiver_sn = ["ACW22120BC9","AGJ2206R6L7"]
+trasceiver_sn = ["ACW22120209","AGJ2206R307"]
 #print(switch_ip["SWCONTDC"])
 
 for z in switch_cisco :
@@ -24,8 +24,8 @@ for z in switch_cisco :
         device_type='cisco_ios',
         host=switch_cisco[z], 
         port=22, 
-        username='AdminSwitch', 
-        password='1n1ct3l2020.'
+        username='cisco', 
+        password='cisco123.'
         )
     comando ="sh inventory "
     output = sshCli.send_command(comando)
