590758456b804ae3d9e3b80944abb2038ac6d146
Cesar Aguirre <cesarinj@hotmail.com>
2020-03-06 17:08:36 -0500

    busca modulo

--- modulo.py
+++ modulo.py
@@ -1,11 +1,9 @@
 ### Identifica en que equipos estan los trasceiver 
 
 from netmiko import ConnectHandler
-
-switch_3com= {  "SW01" : "10.10.10.10",
+switch_hp= {  "SW01" : "10.10.10.10",
           }
-switch_3com1= { "SW02" : "10.10.10.11",
-
+switch_aruba= { "SW02" : "10.10.10.11",
             }
 switch_cisco= { "SW03" : "10.10.10.12",  # 
                 "SW04" : "10.10.10.13",  # 
@@ -13,13 +11,8 @@ switch_cisco= { "SW03" : "10.10.10.12",  #
                 "SW06" : "10.10.10.14", 
                 "SW07" : "10.10.10.15", 
                 }
-    
 trasceiver_sn = ["ACW22120209","AGJ2206R307"]
-#print(switch_ip["SWCONTDC"])
-
 for z in switch_cisco :
-  #  print(z)
-  #  print(switch_cisco[z])
     sshCli = ConnectHandler(
         device_type='cisco_ios',
         host=switch_cisco[z], 
